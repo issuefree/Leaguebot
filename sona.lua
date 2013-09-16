3ff474c98f19c4e96a712e1b746477422e73b314
Tim O'Connor <issuefree@gmail.com>
2013-09-16 02:16:34 -0600

    Added damage during combat and abit more tear charging.

--- sona.lua
+++ sona.lua
@@ -6,33 +6,37 @@ require "support"
 pp("\nTim's Sona")
 AddToggle("lastHit", {on=false, key=112, label="Last Hit", auxLabel="{0}", args={"blue"}})
 AddToggle("healTeam", {on=true, key=113, label="Heal Team", auxLabel="{0}", args={"green"}})
-AddToggle("fastWalk", {on=true, key=114, label="Fast Walk"})
+AddToggle("tear", {on=true, key=114, label="Charge Tear / Fastwalk"})
 
 spells["blue"] = {
    key="Q", 
    range=825, 
    color=blue, 
    base={50,100,150,200,250}, 
-   ap=.7
+   ap=.7,
+   cost={45,50,55,60,65}
 }
 spells["green"] = {
    key="W", 
    range=1000, 
    color=green, 
    base={40,60,80,100,120}, 
-   ap=.25
+   ap=.25,
+   cost={60,65,70,75,80}
 }
 spells["violet"] = {
    key="E", 
    range=999, 
-   color=violet 
+   color=violet,
+   cost=65
 }
 spells["yellow"] = {
    key="R", 
    range=1001, 
    color=yellow, 
    base={150,250,350}, 
-   ap=.8
+   ap=.8,
+   cost={100,150,200}
 }
 
 pcBlue = nil
@@ -45,10 +49,6 @@ function Run()
       return
    end
 
-   if HotKey() then
-      UseItems()
-   end
-     
    if IsOn("healTeam") and CanUse("green") then
       local closeAllies = GetInRange(me, "green", ALLIES)
       local count = 0
@@ -56,18 +56,25 @@ function Run()
          if not IsRecalling(hero) then
             if hero.health + GetSpellDamage(spell) < hero.maxHealth*.66 then
                Cast("green", me)
-               PrintAction("Heal", hero)
+               PrintAction("Heal because I should", hero)
                break
             end            
             if hero.health + GetSpellDamage(spell) < hero.maxHealth*.9 then
                count = count + 1
                if count >= 2 then
                   Cast("green", me)
-                  PrintAction("Heal")
+                  PrintAction("Heal because I can")
                   break
                end
             end
          end
+      end
+   end
+
+   if HotKey() then
+      UseItems()
+      if Action() then
+         return true
       end
    end
    
@@ -78,24 +85,46 @@ function Run()
          if minionRays <= 0 then
             break
          end
-         if minion.health < GetSpellDamage(spell, minion) then
+         if WillKill("blue", minion) then
             Cast("blue", minion)
-            break
+            PrintAction("Blue for lasthit")
+            return true
          end
          minionRays = minionRays - 1
       end
    end
    
-   if IsOn("fastWalk") and CanUse("violet") and Alone() then
-      if GetDistance(HOME) > 1000 and me.mana/me.maxMana > .9 then
-         Cast("violet", me)
-      elseif CanChargeTear() and me.mana/me.maxMana > .75 then
-         Cast("violet", me)
+   if IsOn("tear") then
+      if CanUse("violet") and Alone() then
+         if GetDistance(HOME) > 1000 and GetMPerc(me) > .9 then
+            Cast("violet", me)
+            return true
+         elseif CanChargeTear() and GetMPerc(me) > .75 then
+            Cast("violet", me)
+            return true
+         end
+      end
+
+      if CanUse("blue") and not CanUse("violet") and VeryAlone() then
+         if GetMPerc(me) > .75 and CanChargeTear() then
+            Cast("blue", me)
+            return true
+         end
       end
    end
-   
+
 end
 
+function Action()
+   if CanUse("blue") then
+      local target = GetWeakestEnemy("blue")
+      if target then
+         Cast("blue", me)
+         PrintAction("Blue", target)
+         return true
+      end
+   end
+end
 
 local function onObject(object)
    if find(object.charName, "SonaPowerChordReady") then
