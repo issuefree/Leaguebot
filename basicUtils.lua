3950f30bca571dde4f6a0aec76afaf4a6ee9f157
Tim O'Connor <issuefree@gmail.com>
2013-09-26 00:01:02 -0600

    "class"

--- basicUtils.lua
+++ basicUtils.lua
@@ -1,6 +1,18 @@
 -- globals for convenience
 me = GetSelf()
 
+function class()
+    local cls = {}
+    cls.__index = cls
+    return setmetatable(cls, {__call = function (c, ...)
+        local instance = setmetatable({}, cls)
+        if cls.__init then
+            cls.__init(instance, ...)
+        end
+        return instance
+    end})
+end
+
 local function table_print(tt, indent, done)
    done = done or {}
    indent = indent or 0
