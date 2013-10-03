60c2597199223638cd0f3a7b500b1eb37c8bb143
Tim O'Connor <issuefree@gmail.com>
2013-10-03 01:21:10 -0600

    Corki

--- corki.lua
+++ corki.lua
@@ -13,51 +13,81 @@ AddToggle("lasthit", {on=true, key=116, label="Last Hit", auxLabel="{0}", args={
 AddToggle("clearminions", {on=false, key=117, label="Clear Minions"})
 
 spells["phos"] = {
-  key="Q", 
-  range=600, 
-  color=violet, 
-  base={80,130,180,230,280}, 
-  ap=.5,
-  delay=2,
-  speed=0,
-  radius=300,
-  cost={80,90,100,110,120},
-  noblock=true
+   key="Q", 
+   range=600, 
+   color=violet, 
+   base={80,130,180,230,280}, 
+   ap=.5,
+   delay=2,
+   speed=0,
+   radius=300,
+   cost={80,90,100,110,120},
+   noblock=true
 }
 spells["valk"] = {
-  key="W", 
-  range=800, 
-  color=yellow, 
-  base={150,225,300,375,450}, 
-  ap=1,
-  delay=2,
-  speed=12,
-  width=200,
-  cost=50,
-  noblock=true
+   key="W", 
+   range=800, 
+   color=yellow, 
+   base={150,225,300,375,450}, 
+   ap=1,
+   delay=2,
+   speed=12,
+   width=200,
+   cost=50,
+   noblock=true
 }
 spells["gun"] = {
-  key="E", 
-  range=600, 
-  color=red, 
-  base={20,32,44,56,68}, 
-  bonusAd=.4,
-  cost={60,70,80,90,100}
+   key="E", 
+   range=600, 
+   color=red, 
+   base={20,32,44,56,68}, 
+   bonusAd=.4,
+   cost={60,70,80,90,100}
 }
 spells["barrage"] = {
-  key="R", 
-  range=1225,
-  color=violet,
-  base={120,190,260}, 
-  ap=.3,
-  ad=.2
-  delay=1.5,
-  speed=19,
-  width=80,
-  cost={30,35,40}
+   key="R", 
+   range=1225,
+   color=violet,
+   base={120,190,260}, 
+   ap=.3,
+   ad=.2,
+   delay=1.5,
+   speed=19,
+   width=80,
+   cost={30,35,40},
+   missileTime={12,10,8},
+   name="MissileBarrage"
 }
 
+local missiles = 7
+local mst = time()
+local mCount = 0
+local bigOne = false
+
 function Run()
+   local lvl = GetSpellLevel("R")
+   if lvl > 0 then
+      if me.dead == 1 then
+         missiles = 4
+         mst = time()
+      end
+      if missiles == 7 then
+         mst = time()
+      else
+         local mTime = spells["barrage"].missileTime[lvl] * (1+me.cdr)
+         if time() - mst > mTime then
+            mst = time()
+            missiles = missiles + 1
+         end
+      end
+      if mCount == 2 then
+         bigOne = true
+      end
+   end
+   if bigOne then
+      PrintState(1, "BIGONE")
+   end
+
    if IsRecalling(me) or me.dead == 1 then
       PrintAction("Recalling or dead")
       return true
@@ -74,10 +104,24 @@ function Run()
 	end
 
 	-- auto stuff that should happen if you didn't do something more important
-   if IsOn("lasthit") and CanUse("phos") then
+   if IsOn("lasthit") and Alone() then
+
       if KillMinionsInArea("phos", 3) then
          return true
       end
+
+      if CanUse("barrage") and VeryAlone() and missiles >= 4 then
+         for _,minion in ipairs(SortByHealth(GetUnblocked(me, "barrage", MINIONS))) do
+            if WillKill("barrage", minion) and
+               GetDistance(minion) > spells["AA"].range
+            then
+               CastXYZ("barrage", minion)
+               PrintAction("Barrage for lasthit")
+               return true
+            end
+         end
+      end
+
    end
 
    -- low priority hotkey actions, e.g. killing minions, moving
@@ -91,6 +135,18 @@ function Run()
 end
 
 function Action()
+   if SkillShot("phos") then
+      return true
+   end
+
+   if CanUse("barrage") then
+      if JustAttacked() or not GetWeakestEnemy("AA") then
+         if SkillShot("barrage") then
+            return true
+         end
+      end
+   end
+
    local target = GetMarkedTarget() or GetWeakestEnemy("AA")
    if AA(target) then
       PrintAction("AA", target)
@@ -133,6 +189,14 @@ local function onObject(object)
 end
 
 local function onSpell(unit, spell)
+   if ICast("barrage", unit, spell) then
+      missiles = missiles - 1
+      mCount = mCount + 1
+   end
+   if ICast("MissileBarrageMissile2", unit, spell) then
+      mCount = 0
+      bigOne = false
+   end
 end
 
 AddOnCreate(onObject)
