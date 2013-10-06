698bba41a78e79bbb582eb36d0e6c202a3b33ec0
Tim O'Connor <issuefree@gmail.com>
2013-10-06 14:55:59 -0600

    Just require seems to work fine.

--- modules.lua
+++ modules.lua
@@ -1,21 +1,30 @@
 require "Utils"
+require "spellRanges"
+require "smite"
+require "WardRevealer"
+require "cleanse"
+require "ignite"
+require "antiSkillShot"
+require "objectFind"
+require "testTargets"
+require "support"
 
-local modules = {}
-function loadModule(module)
-	pp("Loading: "..module)
-	local chunk,msg = loadfile(module)      -- loads/compiles the lua chunk
-	if (chunk ~= nil) then
-		chunk()
-	end
-end
+-- local modules = {}
+-- function loadModule(module)
+-- 	pp("Loading: "..module)
+-- 	local chunk,msg = loadfile(module)      -- loads/compiles the lua chunk
+-- 	if (chunk ~= nil) then
+-- 		chunk()
+-- 	end
+-- end
 
---loadModule("clones.lua")
+-- --loadModule("clones.lua")
 
-loadModule("spellRanges.lua")
-loadModule("smite.lua")
-loadModule("WardRevealer.lua")
-loadModule("cleanse.lua")
-loadModule("ignite.lua")
-loadModule("antiSkillShot.lua")
-loadModule("objectFind.lua")
-loadModule("testTargets.lua")
+-- loadModule("spellRanges.lua")
+-- loadModule("smite.lua")
+-- loadModule("WardRevealer.lua")
+-- loadModule("cleanse.lua")
+-- loadModule("ignite.lua")
+-- loadModule("antiSkillShot.lua")
+-- loadModule("objectFind.lua")
+-- loadModule("testTargets.lua")
