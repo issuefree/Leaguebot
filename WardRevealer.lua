4df03b5f77a984b87422b1a1194ead9a9adf9680
Tim O'Connor <issuefree@gmail.com>
2013-11-30 20:11:29 -0700

    Rewrite for simplicity and for trinkets

--- WardRevealer.lua
+++ WardRevealer.lua
@@ -3,36 +3,56 @@ require "timCommon"
 local showWardsFromSpells = true
 local showTimerRadius = 100
 local showVisionRangeKey = 18
-local showSameTeam = false
+local showSameTeam = true
 
-local wardTypes = {
-	sight={color=green, duration=180, sightRange=1350, triggerRange=70},
-	vision={color=violet, duration=180, sightRange=1350, triggerRange=70},
-	shaco={color=red, duration=60, sightRange=690, triggerRange=300},
-	shroom={color=yellow, duration=600, sightRange=405, triggerRange=115},
-   yordle={color=yellow, duration=240, sightRange=150, triggerRange=150},
-   bushwhack={color=yellow, duration=240, sightRange=0, triggerRange=150},
-   -- sapling={color=red, duration=350, sightRange=700, triggerRange=500}
+local types = {
+	 { label="Trinket Ward", color=yellow,
+	   duration=60, sightRange=1350, triggerRange=70,
+	   charName="SightWard", name="YellowTrinket", spellName="RelicSmallLantern" },
+	 { label="Trinket Ward", color=yellow,
+	   duration=120, sightRange=1350, triggerRange=70, 
+	   charName="SightWard", name="YellowTrinketUpgrade", spellName="RelicLantern" },
+	 { label="Sight Ward", color=green, 
+		duration=180, sightRange=1350, triggerRange=70,
+		charName="SightWard", name="SightWard", spellName="SightWard" }, 
+	 { label="Sight Ward", color=green, 
+		duration=180, sightRange=1350, triggerRange=70, 
+		charName="SightWard", name="SightWard", spellName="wrigglelantern" },
+	 { label="Vision Ward", color=violet, 
+		duration=180, sightRange=1350, triggerRange=70, 
+		charName="VisionWard", name="VisionWard", spellName="VisionWard" },
+	 { label="Jack in the Box", color=red, 
+		duration=60, sightRange=690, triggerRange=300,
+		charName="Jack In The Box", name="ShacoBox", spellName="JackInTheBox" },
+	 { label="Shroom", color=red, 
+		duration=600, sightRange=405, triggerRange=115, 
+		charName="Noxious Trap", name="TeemoMushroom", spellName="BantamTrap" },
+  	 { label="Yordle Trap", color = red, 
+  		duration = 240, sightRange=150, triggerRange=150,
+  		charName="Cupcake Trap", name="CaitlynYordleTrap", spellName="CaitlynYordleTrap" }, 
+  	 { label="Bushwhack", color=yellow,
+  		duration = 240, sightRange=0, triggerRange=150,
+		charName="Noxious Trap", name="Nidalee_Spear", spellName="Bushwhack" }
 }
 
 local wardSpots = {
         -- ward spots
-        { x = 2572,    y = 45.84,  z = 7457},     -- BLUE GOLEM
+        { x = 2850,    y = 45.84,  z = 7575},     -- BLUE GOLEM
         { x = 7422,    y = 46.53,  z = 3282},     -- BLUE LIZARD
         { x = 10148,   y = 44.41,  z = 2839},     -- BLUE TRI BUSH
         { x = 6269,    y = 42.51,  z = 4445},     -- BLUE PASS BUSH
         { x = 7406,    y = 43.31,  z = 4995},     -- BLUE RIVER ENTRANCE
-        { x = 4325,    y = 44.38,  z = 7241.54},  -- BLUE ROUND BUSH
+        { x = 4325,    y = 44.38,  z = 7041.54},  -- BLUE ROUND BUSH
         { x = 4728,    y = -51.29, z = 8336},     -- BLUE RIVER ROUND BUSH
         { x = 6598,    y = 46.15,  z = 2799},     -- BLUE SPLIT PUSH BUSH
  
-        { x = 11500,   y = 45.75,  z = 7095},     -- PURPLE GOLEM
+        { x = 11183,   y = 45.75,  z = 6899},     -- PURPLE GOLEM
         { x = 6661,    y = 44.46,  z = 11197},    -- PURPLE LIZARD
         { x = 3883,    y = 39.87,  z = 11577},    -- PURPLE TRI BUSH
         { x = 7775,    y = 43.14,  z = 10046.49}, -- PURPLE PASS BUSH
         { x = 6625.47, y = 47.66,  z = 9463},     -- PURPLE RIVER ENTRANCE
-        { x = 9720,    y = 45.79,  z = 7210},     -- PURPLE ROUND BUSH
-        { x = 9191,    y = -73.46, z = 6004},     -- PURPLE RIVER ROUND BUSH
+        { x = 9658,    y = 45.79,  z = 7556},     -- PURPLE ROUND BUSH
+        { x = 9300,    y = -73.46, z = 6128},     -- PURPLE RIVER ROUND BUSH
         { x = 7490,    y = 41,     z = 11681},    -- PURPLE SPLIT PUSH BUSH
  
         { x = 3527.43, y = -74.95, z = 9534.51},  -- NASHOR
@@ -58,15 +78,15 @@ end
 function drawWards()
 	for i,ward in ipairs(wards) do 
 		local timer = string.format(math.ceil((ward.tick+ward.duration-time())))
-		Circle(ward, ward.triggerRange, ward.color)
+		Circle(ward.loc, ward.triggerRange, ward.color)
 		if showVisionRange then					
-			Circle(ward, ward.sightRange, ward.color)
+			Circle(ward.loc, ward.sightRange, ward.color)
 		end
-		if GetDistance(ward, GetMousePos()) < showTimerRadius then
+		if GetDistance(ward.loc, GetMousePos()) < showTimerRadius then
 			if ward.source == "onload" then
-				DrawText(ward.name..": max "..timer, GetCursorX()-13, GetCursorY()-17, timerColor)
+				DrawText(ward.label..": max "..timer, GetCursorX()-13, GetCursorY()-17, timerColor)
 			else
-				DrawText(ward.name..": "..timer, GetCursorX()-13, GetCursorY()-17, timerColor)
+				DrawText(ward.label..": "..timer, GetCursorX()-13, GetCursorY()-17, timerColor)
 			end
 		end
 	end
@@ -91,60 +111,37 @@ function cleanUpWards()
 end
 
 local function createWards(object)
-	if object.charName == "VisionWard" or 
-		object.charName == "SightWard" or
-		object.charName == "Jack In The Box" or 
-		object.charName == "Noxious Trap" or
-		object.charName == "Cupcake Trap" or
-		object.name == "Nidalee_Spear" --or
-		-- object.charName == "MaokaiSproutling"
-	then
-		local ward = {x=object.x, y=object.y, z=object.z, object=object, tick=time(), source="oncreate"}		
-		if LOADING then
-			ward.source = "onload"
+	for _,type in ipairs(types) do
+		if (type.charName and object.charName == type.charName) or 
+			(type.name and object.name == type.name)
+		then
+			local ward = {loc=Point(object), object=object, tick=time(), source="oncreate"}
+			if LOADING then
+				ward.source = "onload"
+			end
+			addWard(ward, type)
+			break
 		end
-		addWard(ward)
 	end
 end
 
-local function processWards(object,spell)
-	local ward
-	if showSameTeam or object.team ~= me.team then
-		if spell.name == "SightWard" then
-			ward = {name="Sight Ward", source="spell", type="sight"}
-		elseif spell.name == "wrigglelantern" then
-			ward = {name="Sight Ward", source="spell", type="sight"}
-		elseif spell.name == "VisionWard" then
-			ward = {name="Vision Ward", source="spell", type="vision"}
-		elseif spell.name == "ItemGhostWard" then
-			ward = {name="Sight Ward", source="spell", type="sight"}
-		elseif spell.name == "JackInTheBox" then
-			ward = {name="Shaco Box", source="spell", type="shaco"}
-		elseif spell.name == "BantamTrap" then
-			ward = {name="Shroom", source="spell", type="shroom"}
-		elseif spell.name == "CaitlynYordleTrap" then
-			ward = {name="Yordle Trap", source="spell", type="yordle"}
-		elseif spell.name == "Bushwhack" then
-			ward = {name="Bushwhack", source="spell", type="bushwhack"}
-		-- elseif spell.name == "MaokaiSapling2" then
-		-- 	ward = {name="Sapling", source="spell", type="sapling"}
-		else
-			return
+local function processWards(object, spell)
+	if IsHero(object) and (showSameTeam or IsEnemy(object)) then
+		for _,type in ipairs(types) do
+			if type.spellName == spell.name then
+				local ward = {loc=Point(spell.endPos), tick=time(), source="spell"}
+				addWard(ward, type)
+				break
+			end
 		end
-		ward.x = spell.endPos.x
-		ward.y = spell.endPos.y
-		ward.z = spell.endPos.z
-		ward.tick = time()
-		addWard(ward)
 	end
 end
 
-function addWard(ward)
-	ward = populate(ward)
+function addWard(ward, type)
+	ward = merge(ward, type)
 	--check for dups
 	for i,w in rpairs(wards) do
-		if math.abs(w.x - ward.x) < 100 and 
-		   math.abs(w.z - ward.z) < 100 and
+		if GetDistance(w.loc, ward.loc) < 100 and
 		   math.abs(w.tick - ward.tick) < 1 then
 			if ward.source == "spell" then  -- don't add spells if the obj exists
 				return
@@ -155,48 +152,6 @@ function addWard(ward)
 		end
 	end
 	table.insert(wards, ward)
-end
-
-function populate(ward)
-	if ward.object and ward.object.charName == "SightWard" and ward.object.name == "SightWard" then
-		ward.name = "Sight Ward"
-		ward.type = "sight"
-				
-	elseif ward.object and ward.object.charName == "VisionWard" and ward.object.name == "VisionWard" then
-		ward.name = "Vision Ward"
-		ward.type = "vision"
-		
-	elseif ward.object and ward.object.charName == "VisionWard" and ward.object.name == "SightWard" then
-		ward.name = "Explorer's Ward"
-		ward.type = "sight"
-		
-	elseif ward.object and ward.object.charName == "Jack In The Box" then
-		ward.name = "Shaco Box"
-		ward.type = "shaco"
-				
-	elseif ward.object and ward.object.charName == "Cupcake Trap" then
-		ward.name = "Yordle Trap"
-		ward.type = "yordle"
-		
-	elseif ward.object and ward.object.name == "Nidalee_Spear" then
-		ward.name = "Bushwhack"
-		ward.type = "bushwhack"
-		
-	elseif ward.object and ward.object.charName == "Noxious Trap" then
-		ward.name = "Shroom"
-		ward.type = "shroom"
-		
-	elseif ward.object and ward.object.charName == "MaokaiSproutling" then
-		ward.name = "Sapling"
-		ward.type = "sapling"
-		
-	end
-	if not wardTypes[ward.type] then
-		pp(ward.object.charName)
-	else 
-		ward = merge(ward, wardTypes[ward.type])
-		return ward
-	end
 end
 
 AddOnCreate(createWards)
