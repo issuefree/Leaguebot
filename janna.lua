12070993ce6d8bd41eff811cac0b6fb965c4e651
Tim O'Connor <issuefree@gmail.com>
2013-11-30 20:13:22 -0700

    3.14

--- janna.lua
+++ janna.lua
@@ -1,14 +1,40 @@
-require "Utils"
 require "timCommon"
 require "modules"
-require "support"
 
 pp("\nTim's Janna")
 
-spells["gale"]    = {key="Q", range=1700, color=violet, base={60,85,110,135,160}, ap=.75}
-spells["zephyr"]  = {key="W", range=600,  color=violet, base={60,115,170,225,280}, ap=.6}
-spells["eye"]     = {key="E", range=800,  color=green,  base={80,120,160,200,240}, ap=.9}
-spells["monsoon"] = {key="R", range=800, color=green, base={280,440,600}, ap=1.4}
+spells["tailwind"] = {
+	range=800,
+	color=blue
+}
+spells["gale"] = {
+   key="Q",
+	range=1700, 
+	color=violet, 
+	base={60,85,110,135,160}, 
+	ap=.35
+}
+spells["zephyr"] = {
+	key="W", 
+	range=600,  
+	color=violet, 
+	base={60,115,170,225,280}, 
+	ap=.5
+}
+spells["eye"] = {
+	key="E", 
+	range=800,  
+	color=green,  
+	base={80,120,160,200,240}, 
+	ap=.7
+}
+spells["monsoon"] = {
+	key="R", 
+	range=800, 
+	color=green, 
+	base={280,440,600}, 
+	ap=1.4
+}
 
 AddToggle("shield", {on=true, key=112, label="Auto Shield", auxLabel="{0}", args={"eye"}})
 
