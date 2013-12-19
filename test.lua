b324979fa0160dcbfcb15b11fca95a9b9ac980c1
Tim O'Connor <issuefree@gmail.com>
2013-12-18 20:47:34 -0700

    .

--- test.lua
+++ test.lua
@@ -1,83 +1,10 @@
-local function table_print (tt, indent, done)
-   done = done or {}
-   indent = indent or 0
-   if type(tt) == "table" then
-      local sb = {}
-      for key, value in pairs (tt) do
-         table.insert(sb, string.rep (" ", indent)) -- indent it
-         if type (value) == "table" and not done [value] then
-            done [value] = true
-            table.insert(sb, "{\n");
-            table.insert(sb, table_print (value, indent + 2, done))
-            table.insert(sb, string.rep (" ", indent)) -- indent it
-            table.insert(sb, "}\n");
-         elseif "number" == type(key) then
-            table.insert(sb, string.format("\"%s\"\n", tostring(value)))
-         else
-            table.insert(sb, string.format(
-            "%s = \"%s\"\n", tostring (key), tostring(value)))
-         end
-      end
-      return table.concat(sb)
-   else
-      return tt .. "\n"
-   end
+function trunc(num, places)
+   if not places then places = 2 end
+   local factor = 10^places
+   return math.floor(num*factor)/factor
 end
 
-function pp(str)
-   if str == nil then
-      pp("nil")
-   elseif type(str) == "table" then
-      pp(table_print(str, 2))
-   else
-      print(str)
-   end
-end
+local health = 400
+local maxHealth = 600
 
-function concat(...)
-   local resTable = {}
-   for _,tablex in ipairs(arg) do
-      if type(tablex) == "table" then
-         for _,v in ipairs(tablex) do
-            table.insert(resTable, v)
-         end
-      else
-         table.insert(resTable, tablex)
-      end      
-   end
-   return resTable
-end
-
-function foo()
-   return 1, 2
-end
-
-function t(a, b)
-   pp(a.." "..b)
-end
-
-function class()
-    local cls = {}
-    cls.__index = cls
-    return setmetatable(cls, {__call = function (c, ...)
-        local instance = setmetatable({}, cls)
-        if cls.__init then
-            cls.__init(instance, ...)
-        end
-        return instance
-    end})
-end
-Point = class()
-
-function Point:__init(a, b, c)
-   self.x = a
-   self.y = b
-   self.z = c
-end
-
-function Point:__add(a)
-   return Point(self.x+a.x, self.y+a.y, self.z+a.z)
-end
-
-pp(type(Point(1,2,1) + Point(5,2,2)))
-
+print(1+(1-health/maxHealth)/2)
