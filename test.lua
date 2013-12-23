3d0b6b487ffacf3d7398955adc052a53d6e21a4c
Tim O'Connor <issuefree@gmail.com>
2013-12-22 19:05:28 -0700

    .

--- test.lua
+++ test.lua
@@ -1,19 +1,9 @@
+function GetSelf() end
+printtext = print
+require "basicUtils"
 
-function trunc(num, places)
-   if not places then places = 2 end
-   local factor = 10^places
-   return math.floor(num*factor)/factor
-end
 
-function class()
-    local cls = {}
-    cls.__index = cls
-    return setmetatable(cls, {__call = function (c, ...)
-        local instance = setmetatable({}, cls)
-        if cls.__init then
-            cls.__init(instance, ...)
-        end
-        return instance
-    end})
-end
 
+f = Damage(10, "H")
+
+pp(f+1)
