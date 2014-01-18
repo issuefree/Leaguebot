da4791d1c72863d2682414d04e893071855fa4f5
Tim O'Connor <issuefree@gmail.com>
2014-01-18 13:56:32 -0700

    Muramana usage for corki

--- corki.lua
+++ corki.lua
@@ -6,7 +6,7 @@ pp("\nTim's Corki")
 AddToggle("move", {on=true, key=112, label="Move to Mouse"})
 AddToggle("", {on=true, key=113, label=""})
 AddToggle("", {on=true, key=114, label=""})
-AddToggle("", {on=true, key=115, label=""})
+AddToggle("tear", {on=true, key=115, label="Tear"})
 
 AddToggle("lasthit", {on=true, key=116, label="Last Hit", auxLabel="{0}", args={GetAADamage}})
 AddToggle("clearminions", {on=false, key=117, label="Clear Minions"})
@@ -94,6 +94,11 @@ function Run()
       return true
    end
 
+
+   if IsOn("tear") and not P.muramana then
+      UseItem("Muramana", me)
+   end
+
    -- auto stuff that always happen
 
    -- high priority hotkey actions, e.g. killing enemies
@@ -156,6 +161,7 @@ function Action()
 
    return false
 end
+
 function FollowUp()
    if IsOn("lasthit") and Alone() then
       if KillMinion("AA") then
