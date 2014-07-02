d0951b23b5cb88774b9e8def8150758ecaa967d1
Tim O'Connor <issuefree@gmail.com>
2014-07-01 18:38:24 -0600

    Cleanup. Use AutoPet.

--- issuefree/champs/annie.lua
+++ issuefree/champs/annie.lua
@@ -39,6 +39,7 @@ spells["tibbers"] = {
    cost={125,175,225}, 
    ap=.8,
    radius=250,
+   petRange=300,
    cost=100
 }
 
@@ -51,23 +52,22 @@ function stunOn()
    end
 end
 
-AddToggle("", {on=true, key=112, label=""})
+AddToggle("", {on=true, key=112, label="- - -"})
 -- build up and hold on to stun
 AddToggle("stoke", {on=true, key=113, label="Stoke", auxLabel="{0}", args={stunOn}})
 AddToggle("", {on=true, key=114, label=""})
 AddToggle("", {on=true, key=115, label=""})
 
 AddToggle("lasthit", {on=true, key=116, label="Last Hit", auxLabel="{0} / {1} / {2}", args={GetAADamage, "dis", "inc"}})
-
-local tibbersHasTarget = false
-local tibbersRange = 300
+AddToggle("clear", {on=false, key=117, label="Clear Minions"})
+AddToggle("move", {on=true, key=118, label="Move"})
 
 function Run()
    if StartTickActions() then
       return true
    end
 
-   autoTibbers()   
+   AutoPet(P.tibbers)
 
    if P.stun then
       if CheckDisrupt("dis") or
@@ -85,10 +85,11 @@ function Run()
    end
    
    -- if i don't have stun and I have mana and I'm alone, stack stun with shield
-   if IsOn("stoke") and Alone() and
+   if IsOn("stoke") and 
+      Alone() and
       not P.stun and 
       CanUse("shield") and
-      me.mana / me.maxMana > .25 
+      GetMPerc(me) > .25 
    then
       Cast("shield", me)
       PrintAction("Shield for stoke")
@@ -103,20 +104,20 @@ function Run()
    if IsOn("lasthit") then
 
       if VeryAlone() then
-         if KillMinionsInCone(spells["inc"], 2, 0, false) then
+         if KillMinionsInCone("inc", 2) then
             PrintAction("Incinerate 2")
             PauseToggle("lasthit", .5)
             return true
          end
       elseif Alone() then
          if IsOn("stoke") and not P.stun then
-            if KillMinionsInCone(spells["inc"], 2, 0, P.stun) then
+            if KillMinionsInCone(spells["inc"], 2) then
                PrintAction("Incinerate 2")
                PauseToggle("lasthit", .5)
                return true
             end
          else
-            if KillMinionsInCone(spells["inc"], 3, 0, false) then
+            if KillMinionsInCone(spells["inc"], 3) then
                PrintAction("Incinerate 3")
                PauseToggle("lasthit", .5)
                return true
@@ -149,40 +150,6 @@ function Run()
    EndTickActions()
 end
 
-function autoTibbers()
-   -- SpellNameR when not tibbers is "InfernalGuardian"
-   -- SpellNameR when tibbers is "infernalguardianguide"
-   if P.tibbers then
-      Circle(P.tibbers, tibbersRange, red)
-
-      tibbersHasTarget = false
-      -- find the closest target to tibbers
-      local target = GetMarkedTarget() or SortByDistance(GetInRange(P.tibbers, 1000, ENEMIES), P.tibbers)[1]
-      if target then
-         tibbersHasTarget = true
-         tibbersAttack(target)
-      end      
-   else
-      tibbers = nil
-   end
-end
-
-local tibbersAction
-function tibbersAttack(target)
-   if GetDistance(P.tibbers, target) > tibbersRange then
-      CastSpellTarget("R", target)
-      if tibbersAction ~= "CHARGE" then
-         PrintAction("Tibbers CHARGE", target)
-         tibbersAction = "CHARGE"
-      end
-   else
-      if tibbersAction ~= "ATTACK" then
-         PrintAction("Tibbers ATTACK", target)      
-         tibbersAction = "ATTACK"
-      end
-   end
-end
-
 function Action()
 -- actually I think it's simple. just hit whoever I can with disintigrate
 -- and incinerate and rely on movement to get me in range for shit.
@@ -196,7 +163,7 @@ function Action()
 
 -- check for finish offs with tibbers. (handle the one target left kill case)
 
-   if CanUse("tibbers") and me.SpellNameR == "InfernalGuardian" then
+   if CanUse("tibbers") and not P.tibbers then
       local hits, kills, score = GetBestArea(me, "tibbers", 1, 3, ENEMIES)
       if score >= 2 then
          UseItem("Deathfire Grasp", GetWeakest("tibbers", hits))
@@ -212,7 +179,7 @@ function Action()
       -- end
       local target = GetMarkedTarget() or GetWeakestEnemy("inc")
       if target then
-         if ( IsOn("stoke") and not P.stun ) or 
+         if ( IsOn("stoke") and not P.stun ) or -- hold off on incinerate if you're holding a stun and they're at the edge of range
             GetDistance(target) < 525 
          then
             if CanUse("dis") then
@@ -238,7 +205,7 @@ function Action()
    end
 
    if CanUse("tibbers") and me.SpellNameR == "InfernalGuardian" then
-      local hits, kills, score = GetBestArea(me, "tibbers", 1, 5, ENEMIES)
+      local hits, kills, score = GetBestArea(me, "tibbers", .1, 1, ENEMIES)
 
       if #kills >= 1 then
          CastXYZ("tibbers", GetCenter(hits))
@@ -258,22 +225,13 @@ end
 local function onObject(object)
    PersistBuff("stun", object, "StunReady")
 
-   if object.team == me.team then
-      Persist("tibbers", object, "Tibbers")
-   end
+   Persist("tibbers", object, "Tibbers", me.team)
 end
 
 local function onSpell(unit, spell)
    CheckShield("shield", unit, spell)   
 
-   if P.tibbers and 
-      unit.name == me.name and 
-      spell.target and
-      spell.target.team ~= me.team and
-      not tibbersHasTarget
-   then
-      tibbersAttack(spell.target)
-   end
+   CheckPetTarget(P.tibbers, unit, spell)
 end
 
 AddOnCreate(onObject)
