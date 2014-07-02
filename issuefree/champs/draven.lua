0765efe309bd9ca5b9b7128519549bacd7454d76
Tim O'Connor <issuefree@gmail.com>
2014-07-01 18:42:01 -0600

    Update to template.

--- issuefree/champs/draven.lua
+++ issuefree/champs/draven.lua
@@ -3,24 +3,44 @@ require "issuefree/modules"
 
 pp("\nTim's Draven")
 
---AddToggle("healTeam", {on=true, key=112, label="Heal Team", auxLabel="{0}", args={"green"}})
+AddToggle("", {on=true, key=112, label=""})
+AddToggle("", {on=true, key=113, label=""})
+AddToggle("", {on=true, key=114, label=""})
+AddToggle("", {on=true, key=115, label=""})
+
+AddToggle("lasthit", {on=true, key=116, label="Last Hit", auxLabel="{0}", args={GetAADamage}})
+AddToggle("clear", {on=false, key=117, label="Clear Minions"})
+AddToggle("move", {on=true, key=118, label="Move"})
 
 spells["axe"] = {
-  key="Q"
+  key="Q",
+  base=0,
+  ad={.45,.55,.65,.75,.85},
+  type="P",
+  cost="45"
 }
 spells["rush"] = {
-  key="W"
+  key="W",
+  cost=40
 }
 spells["standaside"] = {
   key="E", 
   range=1050, 
   color=violet, 
-  base={70 / 105 / 140 / 175 / 210}, 
+  base={70,105,140,175,210}, 
   adBonus=.5,
-  delay=2,
-  speed=12,
-  width=100,
-  noblock=true
+  delay=2, --?
+  speed=12, --?
+  width=125,
+  noblock=true,
+  cost=70
+}
+spells["death"] = {
+   key="R",
+   base={175,275,375},
+   bonusAd=1.1,
+   type="P",
+   cost=120
 }
 
 
@@ -30,14 +50,45 @@ function Run()
       return true
    end
 
-	if HotKey() then
-		
-	end
+   -- auto stuff that always happen
+   if CheckDisrupt("standaside") then
+      return true
+   end
+
+   -- high priority hotkey actions, e.g. killing enemies
+   if HotKey() and CanAct() then
+      if Action() then
+         return true
+      end
+   end
+   -- auto stuff that should happen if you didn't do something more important
+
+   
+   -- low priority hotkey actions, e.g. killing minions, moving
+   if HotKey() and CanAct() then
+      if FollowUp() then
+         return true
+      end
+   end
 
    EndTickActions()
 end
 
+function Action()
+
+   local target = GetMarkedTarget() or GetWeakestEnemy("AA")
+   if AutoAA(target) then
+      return true
+   end
+
+   return false
+end
+function FollowUp()
+   return false
+end
+
 local function onObject(object)
+   Persist("axe", object, "axe object") -- TODO
 end
 
 local function onSpell(object, spell)
