7f37449ca43127273475fa5e70d3f8654001d6dd
Tim O'Connor <issuefree@gmail.com>
2014-07-07 02:17:00 -0600

    KASSADIN

--- issuefree/champs/kassadin.lua
+++ issuefree/champs/kassadin.lua
@@ -3,132 +3,201 @@ require "issuefree/modules"
 
 print("Tim's Kassadin")
 
-local pulseReady
+SetChampStyle("caster")
+
+AddToggle("execute", {on=false, key=112, label="Execute", auxLabel="", args={}})
+AddToggle("", {on=true, key=113, label=""})
+AddToggle("", {on=true, key=114, label=""})
+AddToggle("", {on=true, key=115, label=""})
+
+AddToggle("lasthit", {on=true, key=116, label="Last Hit", auxLabel="{0}({1}) / {2}", args={GetAADamage, "blade", "sphere"}})
+AddToggle("clear", {on=false, key=117, label="Clear Minions"})
+AddToggle("move", {on=true, key=118, label="Move"})
 
 spells["sphere"] = {
 	key="Q", 
 	range=650, 
-	color=yellow, 
-	base={80,110,140,170,200}, 
+	color=violet, 
+	base={80,105,130,155,180}, 
 	ap=.7,
 	cost={70,75,80,85,90}
 }
+spells["bladePassive"] = {
+	key="W",
+	base=20,
+	ap=.1
+}
+spells["blade"] = {
+	key="W",
+	-- base={40,65,90,115,140},
+	base={20,45,70,95,120},
+	-- ap=.6,
+	ap=.5,
+	range=GetAARange,
+	modAA="blade",
+	object="Kassadin_Base_W_buf"
+}
 spells["pulse"] = {
 	key="E", 
-	range=650, 
-	color=red,
-	base={80,120,160,200,240}, 
-	ap=.7
+	range=700-50, 
+	color=yellow,
+	base={80,105,130,155,180}, 
+	ap=.7,
+	delay=2,
+	speed=0,
+	cone=80,
+	noblock=true
 }
 spells["rift"] = {
-	key="R", 
-	range=700, 
-	color=yellow, 
+	key="R",
+	range=700,
+	color=blue, 
 	base={80,100,120},
-	ap=.8
+	mana=.02,
+	cost=function() return 75*(2^riftStacks) end,
+	bonus=function() return GetSpellDamage("riftStack")*riftStacks end,
+	timeout=12,
+	radius=150+GetWidth(me)
+}
+spells["riftStack"] = {
+	base={40,50,60},
+	mana=.01
 }
 
-function getExecuteLabel()
-	local str = ""
-	local d
-	local dam = 0
-	local spell = spells["sphere"]
-	if CanUse(spell) then
-		d = GetSpellDamage(spell)
-		str = str..d.." / "
-		dam = dam + d
-	else
-		str = str.."-- / "
-	end
-	spell = spells["pulse"]
-	if pulseReady and CanUse(spell) then
-		d = GetSpellDamage(spell)
-		str = str..d.." / "
-		dam = dam + d
-	else
-		str = str.."-- / "
-	end
-	spell = spells["rift"]
-	if CanUse(spell) then
-		d = GetSpellDamage(spell)
-		str = str..d.." / "
-		dam = dam + d
-	else
-		str = str.."-- / "
-	end
-	str = str..dam
-	return str
-end
-
-AddToggle("execute", {on=false, key=112, label="Execute", auxLabel="{0}", args={getExecuteLabel}})
+lastRift = 0
+riftStacks = 0
 
 function Run()
+	-- if P.blade then
+	-- 	spells["AA"].bonus = 0
+	-- else
+		spells["AA"].bonus = GetSpellDamage("bladePassive")
+	-- end
+
+	if time() - lastRift > spells["rift"].timeout then
+		riftStacks = 0
+	end
+
    if StartTickActions() then
       return true
    end
-	
-	if IsKeyDown(hotKey) ~= 0 then
-		if IsOn("execute") then
-			local qDam = 0
-			local eDam = 0
-			local rDam = 0
-	
-			-- try to kill a close enemy
-			local target = GetWeakEnemy("MAGIC", 650)
-			if target then
-				if CanUse("sphere") then qDam = GetSpellDamage("sphere", target) end
-				if CanUse("pulse") and pulseReady then eDam = GetSpellDamage("pulse", target) end
-				if CanUse("rift") then rDam = GetSpellDamage("rift", target) end
-				
-				if qDam + eDam + rDam > target.health then
-					if qDam > 0 then
-						CastSpellTarget("Q", target)
-					elseif eDam > 0 then
-						CastSpellTarget("E", target)
-					elseif rDam > 0 then
-						CastSpellTarget("R", target)
-					end
-				end
-			else
-				qDam = 0
-				eDam = 0
-				rDam = 0
-				-- try to get close to a killable enemy
-				target = GetWeakEnemy("MAGIC", 1350)
-				if target and CanUse("rift") then
-					if CanUse("sphere") then qDam = GetSpellDamage("sphere", target) end
-					if CanUse("pulse") and pulseReady then eDam = GetSpellDamage("pulse", target) end
-					if CanUse("rift") then rDam = GetSpellDamage("rift", target) end
-	
-					if qDam + eDam + rDam > target.health then
-						CastHotkey("SPELLR:WEAKENEMY RANGE=1350 SMARTCAST")
-					end
-				end
-			end
-		end
-		-- just hit shit that's weak
-		local target = GetWeakEnemy('MAGIC',650,"NEARMOUSE")
-		if target then
-			UseItems(target)
-			if CanUse("sphere") then 
-				CastSpellTarget("Q",target) 
-			elseif CanUse("pulse") then
-				CastSpellTarget("E",target)
-			elseif GetDistance(target) < me.range+100 then 
-				CastSpellTarget("W", me)
-				AttackTarget(target) 
-			end
+
+   -- auto stuff that always happen
+   if CheckDisrupt("sphere") then
+      return true
+   end
+
+   -- high priority hotkey actions, e.g. killing enemies
+	if HotKey() and CanAct() then
+		if Action() then
+			return true
 		end
 	end
-	EndTickActions()
+
+	-- auto stuff that should happen if you didn't do something more important
+	if IsOn("lasthit") then
+		if Alone() then
+
+	      if ModAAFarm("blade") then
+	      	return true
+	      end
+
+	   end
+
+	   if VeryAlone() then
+	      if GetMPerc(me) > .5 then
+	      	if CanUse("pulse") and P.pulse then
+	      		if KillMinionsInCone("pulse", 4) then
+	      			return true
+	      		end
+	      	end
+
+	      	if KillMinion("sphere", "far") then
+	      		return true
+	      	end
+	      end
+	   end
+   end
+
+   
+   -- low priority hotkey actions, e.g. killing minions, moving
+   if HotKey() and CanAct() then
+      if FollowUp() then
+         return true
+      end
+   end
+
+   EndTickActions()
 end
 
-function onCreateOb(object)
-	if find(object.charName, "ForcewalkReady") then
-		pulseReady = true
-	elseif find(object.charName, "ForcePulse_tar") then
-		pulseReady = false
+function Action()	
+	if CanUse("rift") then
+		for _,target in ipairs(ENEMIES) do
+			if GetDistance(target) > 650 then -- out of range of normal spells
+				if WillKill("sphere", target) or
+					( WillKill("sphere", "pulse", target) and P.pulse )
+				then
+					CastXYZ("rift", target)
+					PrintAction("Rift close for execute", target)
+					return true
+				end
+			else
+				if WillKill("sphere", target) or
+				   ( WillKill("sphere", "pulse", target) and P.pulse )
+				then
+				else
+					if WillKill("rift", "sphere", "blade", target) or
+						( WillKill("rift", "sphere", "blade", "pulse", target) and P.pulse )
+					then						
+						CastXYZ("rift", target)
+						PrintAction("Rift for execute", target)
+						return true
+					end
+				end
+			end
+		end
 	end
-end 
 
+	if CastBest("sphere") then
+		return true
+	end
+
+	if P.pulse then
+		if CastBest("pulse") then
+			return true
+		end
+	end
+
+   local target = GetMarkedTarget() or GetMeleeTarget()
+   if AutoAA(target, "blade") then
+      return true
+   end
+
+   return false
+end
+function FollowUp()
+	if CanUse("blade") then
+	   if IsOn("move") then
+	      if MeleeMove() then
+	         return true
+	      end
+	   end
+	end
+   return false
+end
+
+local function onCreate(object)
+	PersistBuff("pulse", object, "Kassadin_Base_E_ready_buf")
+end
+
+local function onSpell(unit, spell)
+	if ICast("rift", unit, spell) then
+		lastRift = time()
+		riftStacks = riftStacks + 1
+	end
+end
+
+AddOnCreate(onCreate)
+AddOnSpell(onSpell)
 SetTimerCallback("Run")
+
