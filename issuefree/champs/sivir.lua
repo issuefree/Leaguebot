0d3759f439e645cec1bc04b28d90879e393a24d8
Tim O'Connor <issuefree@gmail.com>
2014-07-13 16:44:42 -0600

    Update

--- issuefree/champs/sivir.lua
+++ issuefree/champs/sivir.lua
@@ -3,11 +3,14 @@ require "issuefree/modules"
 
 print("\nTim's Sivir")
 
-AddToggle("move", {on=true, key=112, label="Move to Mouse"})
+AddToggle("block", {on=true, key=112, label="SpellShield"})
+AddToggle("", {on=true, key=113, label=""})
+AddToggle("", {on=true, key=114, label=""})
+AddToggle("", {on=true, key=115, label=""})
 
 AddToggle("lasthit", {on=true, key=116, label="Last Hit", auxLabel="{0}", args={GetAADamage}})
-AddToggle("block", {on=true, key=113, label="SpellShield"})
 AddToggle("clear", {on=false, key=117, label="Clear Minions"})
+AddToggle("move", {on=true, key=118, label="Move"})
 
 spells["boomerang"] = {
    key="Q", 
@@ -17,13 +20,18 @@ spells["boomerang"] = {
    ap=.5, 
    ad={.7,.8,.9,1,1.1},
    type="P",
-   delay=2,
-   speed=13,
-   width=80,
+   delay=2.4,  -- testskillshot
+   speed=14,   -- testskillshot
+   width=100,
    noblock=true,
    overshoot=-200,
    cost={70,80,90,100,110}
 }
+spells["doubleMinBoomerang"] = copy(spells["boomerang"])
+spells["doubleMinBoomerang"].base = mult(spells["boomerang"].base, .8)
+spells["doubleMinBoomerang"].ad = mult(spells["boomerang"].ad, .8)
+spells["doubleMinBoomerang"].ap = spells["boomerang"].ap*.8
+
 spells["ricochet"] = {
    key="W",
    cost=40,
@@ -44,15 +52,6 @@ function Run()
       return true
    end
 
-   local spell = GetSpell("boomerang")
-
-   -- local hits = GetInLine(me, "boomerang", targets[1], targets)
-   -- for _,t in ipairs(hits) do
-   --    DrawBB(t, red)      
-   -- end
-   -- local c = GetAngularCenter(hits)
-   -- DrawLineObject(me, spell.range, blue, AngleBetween(me, c), spell.width)
-
    if CastAtCC("boomerang") then
       return true
    end
@@ -61,7 +60,30 @@ function Run()
       if Action() then
          return true
       end
-   end   
+   end
+
+   if IsOn("lasthit") then
+      if Alone() then
+
+         if CanUse("boomerang") then
+            local killThresh
+            if GetMPerc(me) > .9 then
+               killThresh = 2
+            elseif GetMPerc(me) > .75 then
+               killThresh = 3
+            elseif GetMPerc(me) > .5 then
+               killThresh = 4
+            else
+               killThresh = 5
+            end
+
+            if KillMinionsInLine("doubleMinBoomerang", killThresh) then
+               return true
+            end
+         end
+
+      end
+   end
 
    if HotKey() and CanAct() then
       if FollowUp() then
@@ -73,27 +95,43 @@ function Run()
 end
 
 function Action()
+   -- TestSkillShot("boomerang")
+
+   if CanUse("ricochet") then      
+      if JustAttacked() and GetWeakestEnemy("AA") then
+         Cast("ricochet", me)
+         PrintAction("Ricochet for aa reset")
+         return true
+      end
+   end
+
+   local target = GetMarkedTarget() or GetWeakestEnemy("AA")
+   if AutoAA(target) then
+      return true
+   end
+
    if SkillShot("boomerang") then
       return true
    end
+
    return false
 end
 
 function FollowUp()
+   if IsOn("clear") then
+      if Alone() then
+         if CanUse("ricochet") and GetMPerc(me) > .5 then
+            if #GetInRange(me, "AA", MINIONS) >= 3 then
+               Cast("ricochet", me)
+               PrintAction("ricochet for clear")
+               return true
+            end
+         end
+      end
+   end
+
    return false
 end
-
--- function getRangTargs()
---    local spell = GetSpell("boomerang")
---    local enemies = SortByDistance(GetInRange(me, "boomerang", ENEMIES))
---    local enemiesFireahead = {}
---    for _,enemy in ipairs(enemies) do
---       local x,y,z = GetFireahead(enemy, spell.delay, spell.speed)
---       table.insert(enemiesFireahead, {x=x, y=y, z=z}         
---    end
-
---    GetInLine(spell.width, enemiesFireahead, )
--- end
 
 local function onSpell(unit, spell)
    if IsOn("block") then
