5a65026b11144be6a0f65727d818ed228173ffeb
Tim O'Connor <issuefree@gmail.com>
2014-08-03 23:10:57 -0600

    Comment

--- issuefree/autoAttackUtil.lua
+++ issuefree/autoAttackUtil.lua
@@ -27,7 +27,7 @@ local latency = ping * 2 / 1000
       --                  attacks = {"attack", "frostarrow"} },
 
 local windup = .4  -- this is the slowest I've seen. Shouldn't ever clip with this but not the most responsive.
-local minMoveTime = .2  -- this seems to work for almost everyone.
+local minMoveTime = .2  -- this seems to work for almost everyone. Lots of people don't need this but it's safer to have something
 
 function GetAARange(target)
    target = target or me
